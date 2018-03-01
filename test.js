e3cad03dceb3a4d17479dd596263c931a757a08d
montywytnom <montywbrown@gmail.com>
2018-02-28 18:51:55 -0800

    created and finished 'uploaded files' project

--- test.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var uc = require('upper-case')
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(uc("hello world"));
-	console.log('hello world');
-	res.end();
-}).listen(8080);
