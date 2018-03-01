c256ef452c6423dc0d3e5df3f072d59d5a21d9a2
montywytnom <montywbrown@gmail.com>
2018-02-28 19:14:33 -0800

    created failing emailer

---
+++ Email/mailer.js
@@ -0,0 +1,21 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: '',
+		pass: '' 
+	}
+});
+
+var mailOptions = {
+	from: 'montywbrown@gmail.com',
+	to: 'montywbrown@gmail.com',
+	subject: 'hey',
+	text: '6668'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+	if (error) {console.log(error)}
+	else {console.log('Email sent: ' + info.response);}
+});
